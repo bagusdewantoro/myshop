3a0c6ebfdbb01947c22d6695558f2be8a38184c1
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-04-21 16:56:03 +0700

    orders

---
+++ orders/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from shop.models import Product
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    
