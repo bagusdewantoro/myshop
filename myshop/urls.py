70e647c514a34d24b659d4b95c963b08bd8690e2
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-04-30 23:02:54 +0700

    style

--- myshop/urls.py
+++ myshop/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "HOPE PROJECT" # modify Admin Site Header
+admin.site.site_title = "HOPE PROJECT" # modify Admin Site Title
+admin.site.index_title = "Hope Administration" # modify Admin Index Title
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
